b74093d9bcf1ae88447dbe9c475b568525f4b799
briandavidjoyner <brianjoyner@Brians-MBP.home>
2017-12-09 19:21:35 -0500

    Fix

--- API/Services/Database/datastore_test.js
+++ API/Services/Database/datastore_test.js
@@ -3,16 +3,21 @@ var MongoClient = require('mongodb').MongoClient;
 var test = require('assert');
 var username = process.env.MONGODB_USER;
 var password = process.env.MONGODB_PASSWORD
-// Connection url
-//var url = 'mongodb://localhost:27017/test';
+var dbName = 'Datastore';
 var url = 'mongodb://' + username + ':' + password + '@mongodb:27017/datastore';
-// Database name
-const dbName = 'Datastore';
 // Connect using MongoClient
 MongoClient.connect(url, function(err, client) {
 	if (err){
 		console.log (err);
 	} else if (client){
 		console.log (client);
+		const database = client.db(dbName);
 	}
 });
+
+//Database Methods
+exports.getDB = function(){
+	return new Promise(function(resolve,reject){
+		resolve (database);
+	});
+}
