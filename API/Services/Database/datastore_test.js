219c7e685f96309c0632c3c70bcf5d9d8f06ac23
briandavidjoyner <brianjoyner@Brians-MBP.home>
2017-12-10 02:00:45 -0500

    Basic Connection

--- API/Services/Database/datastore_test.js
+++ API/Services/Database/datastore_test.js
@@ -1,23 +1,13 @@
 var	Promise = require('promise');
 var MongoClient = require('mongodb').MongoClient;
-var test = require('assert');
+var assert = require('assert');
 var username = process.env.MONGODB_USER;
 var password = process.env.MONGODB_PASSWORD
 var dbName = 'Datastore';
 var url = 'mongodb://' + username + ':' + password + '@mongodb:27017/datastore';
 // Connect using MongoClient
-MongoClient.connect(url, function(err, client) {
-	if (err){
-		console.log (err);
-	} else if (client){
-		console.log (client);
-		const database = client.db(dbName);
-	}
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  db.close();
 });
-
-//Database Methods
-exports.getDB = function(){
-	return new Promise(function(resolve,reject){
-		resolve (database);
-	});
-}
