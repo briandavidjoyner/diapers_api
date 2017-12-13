c5261ab7c98af8bd68e8879873e33e33f10cbe45
briandavidjoyner <brianjoyner@Brians-MBP.home>
2017-12-12 22:28:29 -0500

    Update DB

--- API/Services/Database/datastore_test.js
+++ API/Services/Database/datastore_test.js
@@ -1,13 +1,23 @@
 var	Promise = require('promise');
 var mongoose = require ('mongoose');
-var assert = require('assert');
+var ip = process.env.IP || '0.0.0.0';
 var username = process.env.MONGODB_USER || 'l4HtCfx7b3c3hcuN';
 var password = process.env.MONGODB_PASSWORD || 'l4HtCfx7b3c3hcuN';
 var dbName = 'Datastore';
-var url = 'mongodb://' + username + ':' + password + '@mongodb:27017/datastore_test';
+
+//Comment Out For Production
+//var url = 'mongodb://' + username + ':' + password + '@mongodb:27017/datastore';
+var url = 'mongodb://' + username + ':' + password + '@mongodb-diapers-api.193b.starter-ca-central-1.openshiftapps.com/datastore';
 
 // Connect using MongoClient
 mongoose.connect(url, { useMongoClient: true });
+
+//On Connection Create A New Schema
+var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function(result) {
+  		console.log('connected: ' + result);
+	});
 
 exports.status = function(){
 	return new Promise (function(resolve,reject){
