3bf2ef3b3274853708661b93d1cf612067a1f4d0
briandavidjoyner <brianjoyner@Brians-MBP.home>
2017-12-12 23:20:28 -0500

    Add Item

--- API.js
+++ API.js
@@ -22,4 +22,12 @@ router_API.get('/db', function(req,res){
 	console.log(database.status());
 });
 
+router_API.get('/addItem', function(req,res){
+	database.createItem('cat').then(function(result){
+		res.send(result);
+	}).catch(function(err){
+		res.send(err);
+	});
+});
+
 module.exports = router_API;
