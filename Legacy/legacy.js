9fd875f0760271c1fd016b8ede128cfd8116d368
briandavidjoyner <brianjoyner@Brians-MBP.home>
2018-01-27 16:55:10 -0500

    Fix

--- Legacy/legacy.js
+++ Legacy/legacy.js
@@ -4,7 +4,7 @@ var options;
 var	Promise = require('promise');
 
 router.use('/public', express.static(__dirname + '/public'));
-console.log('1');
+
 router.get('*',function(req,res){
 	res.sendFile(__dirname + '/public/views/pages/angular/main.html');
 });
