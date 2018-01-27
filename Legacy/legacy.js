79953d71a30bddc2013f75babf070faafa5a3326
briandavidjoyner <brianjoyner@Brians-MBP.home>
2018-01-27 16:40:01 -0500

    Legacy update

--- Legacy/legacy.js
+++ Legacy/legacy.js
@@ -3,7 +3,7 @@ var router = express.Router([options]);
 var options;
 var	Promise = require('promise');
 
-router.use('/public', express.static(__dirname + '/public'));
+router.use('/public', express.static(__dirname + '/legacy/public'));
 
 router.get('*',function(req,res){
 	res.sendFile(__dirname + '/public/views/pages/angular/main.html');
