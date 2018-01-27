3a43ab24adf951672ad6ee77c44bd25b39a0c752
briandavidjoyner <brianjoyner@Brians-MBP.home>
2018-01-27 16:54:01 -0500

    Fix To Go Live

--- Legacy/legacy.js
+++ Legacy/legacy.js
@@ -3,8 +3,8 @@ var router = express.Router([options]);
 var options;
 var	Promise = require('promise');
 
-router.use('/public', express.static(__dirname + '/legacy/public'));
-
+router.use('/public', express.static(__dirname + '/public'));
+console.log('1');
 router.get('*',function(req,res){
 	res.sendFile(__dirname + '/public/views/pages/angular/main.html');
 });
