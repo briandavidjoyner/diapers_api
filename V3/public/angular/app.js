f969ead1e9ddd02fcb1ddd51326296037501a008
briandavidjoyner <brianjoyner@Brians-MBP-2.home>
2018-02-26 23:53:56 -0500

    SRC Whitelist

--- V3/public/angular/app.js
+++ V3/public/angular/app.js
@@ -1,3 +1,11 @@
 var diaperApp = angular.module('diaperApp', []);
+
+diaperApp.config(function($sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+    // Allow same origin resource loads.
+    'self',
+    // Allow loading from our assets domain.  Notice the difference between * and **.
+    'https://diapers-diapers.193b.starter-ca-central-1.openshiftapps.com/**'
+  ]);
 	
 console.log('module loaded');
