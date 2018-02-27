92301dfe1a96a3de3ae5d8bad482eb709689223f
briandavidjoyner <brianjoyner@Brians-MBP-2.home>
2018-02-27 00:55:09 -0500

    Base URL Test

--- V3/public/angular/components.js
+++ V3/public/angular/components.js
@@ -1,10 +1,15 @@
-diaperApp.directive('header', function () {
-    console.log('header');
+//URLS
+var base_url = 'https://diapers-diapers.193b.starter-ca-central-1.openshiftapps.com';
+var url ={
+	top_item : base_url + '/v3/public/angular/components/top_item.html'
+}
+
+diaperApp.directive('top_item', function () {
     return {
         restrict: 'E',
         replace: true,
         //scope: {user: '='},
-        templateUrl: 'https://diapers-diapers.193b.starter-ca-central-1.openshiftapps.com/v3/public/angular/components/header.html',
+        templateUrl: url.header,
         //controller: 'diapers'
     }
 });
