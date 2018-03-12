0ad02b67693fc97935d3eab4608ebbd284402a9a
briandavidjoyner <brianjoyner@Brians-MBP-2.home>
2018-03-11 22:22:27 -0400

    End Of Day

--- API/Services/Lookups/Amazon/amazon_standards.js
+++ API/Services/Lookups/Amazon/amazon_standards.js
@@ -12,21 +12,28 @@ exports.Standardize = function(type,input){
 	var processed = 0;
 
 	return new Promise(function(resolve,reject){
+		
 		try {
-			for (var i in input){
 
-				if (type == 'diapers'){
-					if (input[i].ItemAttributes.Brand != undefined &&
+			if (type == 'diapers'){
+
+				for (i=0; i<end; i++) {
+
+					processed ++;
+					console.log(processed);
+
+					if (i = end){
+						resolve (output);
+					} 
+
+					/*if (input[i].ItemAttributes.Title.match(/\wipe/g).length > 1 &&
+						input[i].ItemAttributes.Brand != undefined &&
 						input[i].ItemAttributes.Brand == 'Huggies' ||
 						input[i].ItemAttributes.Brand == 'huggies' ||
 						input[i].ItemAttributes.Brand == 'Pampers' ||
 						input[i].ItemAttributes.Brand == 'pampers' ||
 						input[i].ItemAttributes.Brand == 'Luvs' ||
 						input[i].ItemAttributes.Brand == 'luvs' &&
-						input[i].ItemAttributes.Title.indexOf('ipe') < -1 &&
-						//input[i].ItemAttributes.Title.indexOf('Wipe') < -1 &&
-						//input[i].ItemAttributes.Title.indexOf('wipes') < -1 &&
-						//input[i].ItemAttributes.Title.indexOf('Wipes') < -1 &&
 					 	input[i].DetailPageURL != undefined &&
 					 	input[i].LargeImage != undefined &&
 					 	input[i].OfferSummary.LowestNewPrice.Amount != undefined &&
@@ -40,7 +47,8 @@ exports.Standardize = function(type,input){
 					 	input[i].ItemAttributes.Size.indexOf('Lbs') < 0 &&
 					 	Number.isInteger(parseInt(input[i].ItemAttributes.Title.match(/\d+(?=\s\wount)/))) == true &&
 					 	input[i].ItemAttributes.Size.length < 10
-						) { 
+						) {
+						*/
 							output.push({
 								type: 'diapers',
 								title: input[i].ItemAttributes.Title,
@@ -53,20 +61,29 @@ exports.Standardize = function(type,input){
 								units: parseInt(input[i].ItemAttributes.Title.match(/\d+(?=\s\wount)/)),
 								pricePerUnit: ((input[i].OfferSummary.LowestNewPrice.Amount / 100)/(input[i].ItemAttributes.Title.match(/\d+(?=\s\wount)/))).toFixed(2),
 								size: input[i].ItemAttributes.Size
-								}); 
-							}
+								});
 
-				} else { reject ('Type is not defined'); } //Is this the way to handle this?
+							
+							
+	
 
-			processed ++;
+						/*} else { 
 
-			if (processed == end){ resolve (output); }
+							processed ++;
+							console.log(processed);	
+							continue; 
 
-			}
+						}*/
+				}
+
+			} else { throw new Error ('Type is not defined'); }
 
 		} catch (err) {
-			reject (err);
+			throw new Error ('Something is wrong with validation');
 		}
+	
+	}).catch(function(err){
+		reject (err);
 	});
 }
 
