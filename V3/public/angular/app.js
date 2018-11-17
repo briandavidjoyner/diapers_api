942c0cd970f11b36430a4192f9f72fa9e6b5bd2b
briandavidjoyner <brianjoyner@brians-mbp-4.attlocal.net>
2018-11-17 17:54:30 -0500

    Updating SCE Whitelist

--- V3/public/angular/app.js
+++ V3/public/angular/app.js
@@ -4,11 +4,11 @@ diaperApp.config(function($sceDelegateProvider) {
   $sceDelegateProvider.resourceUrlWhitelist([
     // Allow same origin resource loads.
     'self',
-    // Allow loading from our assets domain.  Notice the difference between * and **.
-    'https://nodejs-mongo-persistent-diapers.193b.starter-ca-central-1.openshiftapps.com//**',
-    'https://diapersdiapers.com/**',
-    'https://www.diapersdiapers.com/**',
-    'diapersdiapers.com/**'
+    '*'
+    //'https://nodejs-mongo-persistent-diapers.193b.starter-ca-central-1.openshiftapps.com//**',
+    //'https://diapersdiapers.com/**',
+    //'https://www.diapersdiapers.com/**',
+    //'diapersdiapers.com/**'
   ]);
  });
 
